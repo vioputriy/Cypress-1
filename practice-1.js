bc133dc1359449bc19fab253774f5dd968f3623f
vio putri yuwandika <46666598+vioputriy@users.noreply.github.com>
2021-08-25 15:22:14 +0700

    Add files via upload

---
+++ practice-1.js
@@ -0,0 +1,20 @@
+describe('', function(){
+    it('tc-1',function(){
+        cy.visit('https://demoqa.com/automation-practice-form')
+        cy.get('#firstName').type('vio')
+        cy.get('#lastName').type('yuwandika')
+        cy.get('#userEmail').type('vioputriyuwan@gmail.com')
+        cy.get('#userNumber').type('081111111111')
+        cy.get('#currentAddress').type('desa hijau')
+        cy.get('#submit').click()
+        cy.get('#genterWrapper').check()
+    })
+})
+
+
+
+
+
+Cypress.on('uncaught:exception', (err, runnable)=>{
+    return false
+})
