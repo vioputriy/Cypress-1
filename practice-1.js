9055606921f0abbd38b863b88b9775dad34c9d50
vio putri yuwandika <46666598+vioputriy@users.noreply.github.com>
2021-08-25 20:21:49 +0700

    Update practice-1.js

--- practice-1.js
+++ practice-1.js
@@ -1,10 +1,12 @@
 describe('', function(){
     it('tc-1',function(){
-        cy.visit('https://demoqa.com/automation-practice-form')
+       cy.visit('https://demoqa.com/automation-practice-form')
         cy.get('#firstName').type('vio')
         cy.get('#lastName').type('yuwandika')
         cy.get('#userEmail').type('vioputriyuwan@gmail.com')
+        cy.get('#genterWrapper > .col-md-9 > :nth-child(2)').click()
         cy.get('#userNumber').type('081111111111')
+        cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(3)').click()
         cy.get('#currentAddress').type('desa hijau')
         cy.get('#submit').click()
         cy.get('#genterWrapper').check()
