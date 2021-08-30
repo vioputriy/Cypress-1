eea9bc72381437747a60786f992cf9a88e697d33
vio putri yuwandika <46666598+vioputriy@users.noreply.github.com>
2021-08-30 21:37:59 +0700

    Update practice-4.js

--- practice-4.js
+++ practice-4.js
@@ -7,7 +7,7 @@ describe('Beli', function () {
         cy.get('#loginusername').type('vio',{force:true}).should('have.value', 'vio');
         cy.get('#loginpassword').type('password.',{force:true}).should('have.value', 'password.');
         cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
-        cy.get('#nameofuser').should('have.text', 'Welcome Dojo-arjen1324');
+        cy.get('#nameofuser').should('have.text', 'Welcome vio');
         cy.contains('HTC One M9',{force:true}).click();
         cy.get('.col-sm-12 > .btn',{force:true}).click();
         cy.get(':nth-child(4) > .nav-link',{force:true}).click();
@@ -24,12 +24,12 @@ describe('Beli', function () {
         cy.get('.col-lg-1 > .btn',{force:true}).click();
         cy.wait(3000);
         cy.get('#orderModalLabel',{force:true}).should('have.text', 'Place order');
-        cy.get('#name',{force:true}).type('Dojo-arjen1324').should('have.value', 'Dojo-arjen1324');
+        cy.get('#name',{force:true}).type('vio').should('have.value', 'vio');
         cy.get('#country',{force:true}).type('Indonesia').should('have.value', 'Indonesia');
-        cy.get('#city',{force:true}).type('Yogyakarta').should('have.value', 'Yogyakarta');
-        cy.get('#card',{force:true}).type('121323612813613').should('have.value', '121323612813613');
+        cy.get('#city',{force:true}).type('Malang').should('have.value', 'Malang');
+        cy.get('#card',{force:true}).type('081234567890').should('have.value', '081234567890');
         cy.get('#month',{force:true}).type('August').should('have.value', 'August');
-        cy.get('#year',{force:true}).type('2022').should('have.value', '2022');
+        cy.get('#year',{force:true}).type('2021').should('have.value', '2021');
         cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary',{force:true}).click();
         cy.get('.sweet-alert > h2',{force:true}).should('have.text', 'Thank you for your purchase!');
         cy.get('.confirm',{force:true}).click();
